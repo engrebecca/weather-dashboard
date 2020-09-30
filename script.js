e6c3a12f76cfbafb6dcdddc40a67c7531ca8abce
reng <67654603+engrebecca@users.noreply.github.com>
2020-09-29 18:00:47 -0700

    Add functionality to save city list to local storage

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 
 // Render city search history
-var cityList = ["New York", "San Francisco", "Seattle"];
+var initialList = JSON.parse(localStorage.getItem("city list"))
+if (initialList === null){
+    var cityList = ["New York", "Melbourne", "Oslo"];
+    localStorage.setItem("city list", JSON.stringify(cityList));
+} else{
+    cityList = initialList;
+}
+// var cityList = ["New York", "Melbourne", "Oslo"];
+// localStorage.setItem("city list", JSON.stringify(cityList));
+
 function renderCities() {
     $("#city-list").empty();
-    for (var i = 0; i < cityList.length; i++) {
-        var cityName = cityList[i];
+    var newCityList = JSON.parse(localStorage.getItem("city list"));
+    for (var i = 0; i < newCityList.length; i++) {
+        var cityName = newCityList[i];
         var cityButton = $(`<li class="list-group-item">${cityName}</li>`);
         cityButton.data("city", cityName);
         $("#city-list").append(cityButton);
@@ -13,17 +23,15 @@ function renderCities() {
 renderCities();
 
 // Push new search to city list array
-function appendCity() {
-    var cityAppend = "Austin";
-    console.log("append" + cityAppend);
-    cityList.push(cityAppend);
+function appendCity(city) {
+    cityList.push(city);
+    localStorage.setItem("city list", JSON.stringify(cityList));
     renderCities();
 }
-appendCity();
 
-// Search for a new city, display current and future conditions, and add to search history
+
 var renderSearchResult = function(city) {
-    // event.preventDefault();
+    // Search for a new city, display current and future conditions, and add to search history
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=b45e06127671f741ee914cedb135bb5d`;
     $.ajax({
         url: queryURL,
@@ -47,123 +55,59 @@ var renderSearchResult = function(city) {
         $("#humidityEl").text(`Humidity: ${humidity} %`);
         $("#windSpeedEl").text(`Wind Speed: ${windSpeed} MPH`);
 
+        // Display UV index for city search input
         var coordURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=b45e06127671f741ee914cedb135bb5d`;
         console.log(coordURL);
-        function returnUV() {
-            $.ajax({
-                url: coordURL,
-                method: "GET"
-            }).then(function (response) {
-                console.log(response);
-                var uvIndex = response.value;
-                console.log(uvIndex);
-                $("#uvEl").text(`UV Index: ${uvIndex}`)
-                if (uvIndex <= 3){
-                    $("#uvEl").html(`UV Index: <span class="bg-success">${uvIndex}</span>`);
-                } else if (uvIndex > 3 && uvIndex <=8){
-                    $("#uvEl").html(`UV Index: <span class="bg-warning">${uvIndex}</span>`);
-                } else {
-                    $("#uvEl").html(`UV Index: <span class="bg-danger">${uvIndex}</span>`);
-                }
-                $("#current-weather").append(uvEl);
-            })
-        }
-        returnUV();
+        $.ajax({
+            url: coordURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            var uvIndex = response.value;
+            console.log(uvIndex);
+            $("#uvEl").text(`UV Index: ${uvIndex}`)
+            if (uvIndex <= 3){
+                $("#uvEl").html(`UV Index: <span class="bg-success">${uvIndex}</span>`);
+            } else if (uvIndex > 3 && uvIndex <=8){
+                $("#uvEl").html(`UV Index: <span class="bg-warning">${uvIndex}</span>`);
+            } else {
+                $("#uvEl").html(`UV Index: <span class="bg-danger">${uvIndex}</span>`);
+            }
+            $("#current-weather").append(uvEl);
+        })
 
-        function renderForecast() {
-            var forecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude={part}&appid=b45e06127671f741ee914cedb135bb5d`;
-            $.ajax({
-                url: forecastURL,
-                method: "GET"
-            }).then(function (response) {
-                console.log("forecast:" + response)
-                // current visibility can be pulled from here
-                for (var i = 1; i < 6; i++) {
-                    var cardCol = $(`<div class="col-lg-2">`)
-                    var newCard = $(`<div class="card bg-primary text-white" style="width: 10rem;">`)
-                    var cardBody = $(`<div class="card-body">`)
-                    var fDate = response.daily[i].dt;
-                    var fdateObject = new Date(fDate * 1000);
-                    var fdateFormat = fdateObject.toLocaleDateString();
-                    var fCondition = response.daily[i].weather[0].icon;
-                    var fTemp = Math.floor(response.daily[i].temp.day);
-                    var fHumidity = response.daily[i].humidity;
-                    var fDateEl = $(`<p>${fdateFormat}</p>`);
-                    var fConditionEl = $(`<img class="h-50 w-50" src="http://openweathermap.org/img/wn/${fCondition}@2x.png">`);
-                    var fTempEl = $(`<p>Temp: ${fTemp} °F</p>`);
-                    var fHumidityEl = $(`<p>Humidity: ${fHumidity} %</p>`);
-                    cardBody.append(fDateEl, fConditionEl, fTempEl, fHumidityEl);
-                    newCard.append(cardBody);
-                    cardCol.append(newCard);
-                    $("#forecast").append(cardCol);
-                }
-
-            })
-        }
-        renderForecast();
+        // Display 5 day forecast for city search input
+        var forecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude={part}&appid=b45e06127671f741ee914cedb135bb5d`;
+        $.ajax({
+            url: forecastURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log("forecast:" + response)
+            $("#forecast").empty();
+            for (var i = 1; i < 6; i++) {
+                var cardCol = $(`<div class="col-lg-2">`)
+                var newCard = $(`<div class="card bg-primary text-white" style="width: 10rem;">`)
+                var cardBody = $(`<div class="card-body">`)
+                var fDate = response.daily[i].dt;
+                var fdateObject = new Date(fDate * 1000);
+                var fdateFormat = fdateObject.toLocaleDateString();
+                var fCondition = response.daily[i].weather[0].icon;
+                var fTemp = Math.floor(response.daily[i].temp.day);
+                var fHumidity = response.daily[i].humidity;
+                var fDateEl = $(`<p>${fdateFormat}</p>`);
+                var fConditionEl = $(`<img class="h-50 w-50" src="http://openweathermap.org/img/wn/${fCondition}@2x.png">`);
+                var fTempEl = $(`<p>Temp: ${fTemp} °F</p>`);
+                var fHumidityEl = $(`<p>Humidity: ${fHumidity} %</p>`);
+                cardBody.append(fDateEl, fConditionEl, fTempEl, fHumidityEl);
+                newCard.append(cardBody);
+                cardCol.append(newCard);
+                $("#forecast").append(cardCol);
+            }
+        })
     })
-    // $.when(returnConditions()).then(returnUV);
 }
 
-// Display UV Index
-// var lat = 30.27;
-// var lon = -97.74;
-// var coordURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=b45e06127671f741ee914cedb135bb5d`;
-// function returnUV (){
-//     $.ajax({
-//         url: coordURL,
-//         method: "GET"
-//     }).then(function(response){
-//         console.log(response);
-//         var uvIndex = response.value;
-//         console.log("UV: " + uvIndex);
-//         if (uvIndex <= 3){
-//             var uvEl = $(`<p>UV Index: <span class="bg-success">${uvIndex}</span></p>`)
-//         } else if (uvIndex > 3 && uvIndex <=8){
-//             var uvEl = $(`<p>UV Index: <span class="bg-warning">${uvIndex}</span></p>`)
-//         } else {
-//             var uvEl = $(`<p>UV Index: <span class="bg-danger">${uvIndex}</span></p>`)
-//         }
-//         $("#current-weather").append(uvEl);
-//     })
-// }
-// returnUV();
-
-// Display 5 day forecast
-// var lat = 30.27;
-// var lon = -97.74;
-// function renderForecast(){
-//     var forecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude={part}&appid=b45e06127671f741ee914cedb135bb5d`;
-//     $.ajax({
-//         url: forecastURL,
-//         method: "GET"
-//     }).then(function(response){
-//         console.log(response)
-//         // current visibility can be pulled from here
-//         for (var i = 1; i < 6; i++){
-//             var cardCol = $(`<div class="col-lg-2">`)
-//             var newCard = $(`<div class="card bg-primary text-white" style="width: 10rem;">`)
-//             var cardBody = $(`<div class="card-body">`)
-//             var fDate = response.daily[i].dt;
-//             var fdateObject = new Date(fDate * 1000);
-//             var fdateFormat = fdateObject.toLocaleDateString();
-//             var fCondition = response.daily[i].weather[0].icon;
-//             var fTemp = Math.floor(response.daily[i].temp.day);
-//             var fHumidity = response.daily[i].humidity;
-//             var fDateEl = $(`<p>${fdateFormat}</p>`);
-//             var fConditionEl = $(`<img class="h-50 w-50" src="http://openweathermap.org/img/wn/${fCondition}@2x.png">`);
-//             var fTempEl = $(`<p>Temp: ${fTemp} °F</p>`);
-//             var fHumidityEl = $(`<p>Humidity: ${fHumidity} %</p>`);
-//             cardBody.append(fDateEl, fConditionEl, fTempEl, fHumidityEl);
-//             newCard.append(cardBody);
-//             cardCol.append(newCard);
-//             $("#forecast").append(cardCol);
-//         }
-
-//     })
-// }
-// renderForecast();
-
+// On click event handlers
 $("#submit-button").on("click", function(event) {
     event.preventDefault();
     var city = $("#search-bar").val();
@@ -171,9 +115,11 @@ $("#submit-button").on("click", function(event) {
         return;
     }
     renderSearchResult(city);
+    appendCity(city);
 });
+
 $("#city-list").on("click", "li", function(event) {
     event.preventDefault();
-    var city = $(event.target).data("city");
+    var city = $(this).data("city");
     renderSearchResult(city);
 });
